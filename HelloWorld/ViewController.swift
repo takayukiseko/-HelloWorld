560febb1713711fda6faf2aca0c64102a1ce807d
taka.seko <sekotaka.0613@gmail.com>
2020-02-03 16:18:31 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 瀬古貴之 on 2020/02/03.
+//  Copyright © 2020 takayuki.seko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
